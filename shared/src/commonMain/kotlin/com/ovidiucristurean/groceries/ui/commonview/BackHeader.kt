03cb0ef73d9c8332ebcf9a9ff00a94d47232d387
Ovidiu Cristurean <ovidiucristurean@Ovidius-MacBook-Pro.local>
2024-02-16 10:13:32 +0200

    fix back arrow color

--- shared/src/commonMain/kotlin/com/ovidiucristurean/groceries/ui/commonview/BackHeader.kt
+++ shared/src/commonMain/kotlin/com/ovidiucristurean/groceries/ui/commonview/BackHeader.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.RenderVectorGroup
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -26,11 +29,23 @@ fun BackHeader() {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
     ) {
+        val image = remember { Icons.Default.ArrowBackIos }
         Image(
-            modifier = Modifier.height(80.dp).clickable {
+            modifier = Modifier.height(80.dp)
+                .clickable {
                 navigator.pop()
             },
-            painter = rememberVectorPainter(Icons.Default.ArrowBackIos),
+            painter = rememberVectorPainter(
+                defaultWidth = image.defaultWidth,
+                defaultHeight = image.defaultHeight,
+                viewportWidth = image.viewportWidth,
+                viewportHeight = image.viewportHeight,
+                name = image.name,
+                tintColor = MaterialTheme.colorScheme.onBackground,
+                tintBlendMode = image.tintBlendMode,
+                autoMirror = image.autoMirror,
+                content = { _, _ -> RenderVectorGroup(group = image.root) }
+            ),
             contentDescription = null
         )
     }
